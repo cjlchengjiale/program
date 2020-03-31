c570f34b0ad53510a23c72254ae3c9012ea16444
cjlchengjiale <1418420512@qq.com>
2020-03-31 10:00:16 +0800

    find_n_max_prime_number&sum_from_N

---
+++ find_prime_number.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#define maxn 100000
+using namespace std;
+bool compare(int a,int b)
+{
+	return a>b;
+}
+int main()
+{
+	long long m,k,sum=0,n=0,num=0,i=0,a[maxn];
+	cin >> m >> k;
+	memset(a,sizeof(a),0);
+	for(int j=2;j<=m;j++)
+	{
+		n=0;
+		for(int k=2;k<j;k++)
+		{
+			if(j%k==0)
+			{
+				n++;
+				if(n==1) continue;
+			}
+		}
+		if(n==0)
+		{
+			a[i++]=j;
+		}
+	}
+	sort(a,a+m,compare);
+	for(int i=0;i<k;i++)
+	{
+		sum+=a[i];
+		if(a[i]!=0)
+		{
+			cout << a[i];
+			if(a[i+1]!=0&&i!=k-1) cout << "+";
+		}
+		if(a[i]==0||i==k-1)
+		{
+			cout << "=" << sum;
+			break;
+		}
+	}
+	return 0;
+}
