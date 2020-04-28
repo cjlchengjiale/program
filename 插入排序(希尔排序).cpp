a85ad9771c4ab2e0f8937cfe06b7df27f320d010
cjlchengjiale <1418420512@qq.com>
2020-04-28 13:45:25 +0800

    插入排序&希尔排序

---
+++ 插入排序(希尔排序).cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+//插入排序
+void insertsort(int* array,int size)
+{
+  int end = 0;
+  int key = 0;
+  for(int i = 0;i<size-1;i++)
+  {
+    end = i;//已排好序列的最后一个元素
+    key = array[end+1];//待排序列的第一个元素
+    //元素移动
+    while(end>=0&&key<array[end])
+    {
+      array[end+1] = array[end];
+      end--;
+    }
+    //插入
+    array[end+1] = key;
+  }
+}
+//希尔排序
+void shellsort(int* array,int size)
+{
+  int gap = size/2;
+  int end = 0,key = 0;
+  //以步调gap进行多轮排序
+  while(gap>1)
+  {
+    gap/=2;//确保最后一轮gap=1
+    //插入排序
+    for(int i = 0;i<size-gap;i++)
+    {
+      end = i;
+      key = array[end+gap];
+      while(end>=0&&key<array[end])
+      {
+        array[end+gap] = array[end];
+        end-=gap;
+      }
+      array[end+gap] = key;
+    }
+  }
+}
+void printnum(int* array,int size)
+{
+  for(int i = 0;i<size;i++)
+  {
+    printf("%d ",array[i]);
+  }
+  printf("\n");
+}
